fb7a9cc5ad1c85feaa27594504a6b5102cda6c98
Evgenii Nikitin <en919@nyu.edu>
2019-05-07 21:50:30 -0400

    rename files for better intuition

--- dlfinalproject/models_rotation/train_supervised.py
+++ dlfinalproject/models_rotation/train_supervised.py
@@ -1,6 +1,6 @@
 import argparse
 
-from dlfinalproject.models_rotation.train_model import train_model
+from dlfinalproject.models_rotation.traner_supervised import train_model
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train Watermark model')
@@ -10,17 +10,18 @@ if __name__ == '__main__':
     parser.add_argument('--decay', type=float, default=0.0)
     parser.add_argument('--image_folders', type=str,
                         nargs='+', default=['unsupervised'])
-    parser.add_argument('--test_size', type=float, default=0.1)
-    parser.add_argument('--random_state', type=int, default=24)
     parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument('--n_epochs', type=int, default=500)
-    parser.add_argument('--eval_interval', type=int, default=1000)
-    parser.add_argument('--model_file', type=str, default='unsupervised.pth')
+    parser.add_argument('--eval_interval', type=int, default=500)
+    parser.add_argument('--model_file', type=str, default='clf.pth')
     parser.add_argument('--restart_optimizer', action='store_true')
     parser.add_argument('--early_stopping', type=int, default=25)
     parser.add_argument('--run_uuid', type=str, default=None)
+    parser.add_argument('--finetune', type=str, default=None)
+    parser.add_argument('--augmentation', type=str, default=None)
     parser.add_argument('--architecture', type=str, default='resnet50')
     parser.add_argument('--filters_factor', type=int, default=4)
+    parser.add_argument('--ignore_best_acc', action='store_true')
     parser.add_argument('--optim', type=str, default='adam')
     parser.add_argument('--momentum', type=float, default=0.0)
     parser.add_argument('--patience', type=int, default=7)
